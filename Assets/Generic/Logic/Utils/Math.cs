41fc4c5f20ff0cae287e7d61c2176c0fd0b1b516
Alex Hirsch <w4rh4wk@bluephoenix.at>
2020-09-15 14:55:50 +0200

    Add Vector2.AsVector3

--- Assets/Generic/Logic/Utils/Math.cs
+++ Assets/Generic/Logic/Utils/Math.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+static class Vector2Extensions
+{
+    public static Vector3 AsVector3(this Vector2 v) => new Vector3(v.x, v.y);
+}
+
 static class Vector3Extensions
 {
     public static Vector2 AsVector2(this Vector3 v) => new Vector2(v.x, v.y);
