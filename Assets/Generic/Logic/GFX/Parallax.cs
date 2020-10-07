5a184835f327f3c50b49f1c03fd8bd48a84164a5
Alex Hirsch <w4rh4wk@bluephoenix.at>
2020-10-07 17:40:18 +0200

    Use localPosition for parallax effect

--- Assets/Generic/Logic/GFX/Parallax.cs
+++ Assets/Generic/Logic/GFX/Parallax.cs
@@ -3,16 +3,10 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     public Vector2 Factor;
-    Vector2 Offset;
-
-    void Awake()
-    {
-        Offset = transform.position;
-    }
 
     void Update()
     {
-        var pos = Factor * Camera.main.transform.position + Offset;
-        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        var pos = Factor * Camera.main.transform.localPosition;
+        transform.localPosition = new Vector3(pos.x, pos.y, transform.localPosition.z);
     }
 }
