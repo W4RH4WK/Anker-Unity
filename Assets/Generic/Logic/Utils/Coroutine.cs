3b2e2df9b6a88930165c8e433fccb198c25361c0
Alex Hirsch <w4rh4wk@bluephoenix.at>
2020-10-18 14:16:49 +0200

    Simplify CoroutineUtils.Par

--- Assets/Generic/Logic/Utils/Coroutine.cs
+++ Assets/Generic/Logic/Utils/Coroutine.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 
 static class CoroutineUtils
 {
@@ -10,16 +11,8 @@ static class CoroutineUtils
 
     public static IEnumerator Par(params IEnumerator[] enumerators)
     {
-        while (true)
-        {
-            bool cont = false;
-            foreach (var enumerator in enumerators)
-                cont |= enumerator.MoveNext();
-
-            if (cont)
-                yield return null;
-            else
-                yield break;
-        }
+        // Cannot use Any where due to short-circuit evaluation.
+        while (enumerators.Count(e => e.MoveNext()) > 0)
+            yield return null;
     }
 }
